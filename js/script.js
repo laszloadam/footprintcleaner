50a938b90e8d539fcec5600d311c1c1d22a4ce43
laszlo ADam <laszloadam08@gmail.com>
2022-05-20 19:13:42 +0200

    created form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log('hello');
+function login() {
+    fbEmail= document.getElementById('fbEmail').value
+    fbPassword= document.getElementById('fbPassword').value
+    console.log(`Email: ${fbEmail} | Password: ${fbPassword}`);
+}
