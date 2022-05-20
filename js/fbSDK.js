dba4aab8231a03c9a49f448d98e3401e6810290b
laszlo ADam <laszloadam08@gmail.com>
2022-05-20 23:44:30 +0200

    add facebook login

---
+++ js/fbSDK.js
@@ -0,0 +1,36 @@
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '733089071220758',
+        cookie: true,
+        xfbml: true,
+        version: 'v13.0'
+    });
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response){
+    if(response.status === 'connected'){
+      console.log('Logged in and authenticated');
+    } else {
+      console.log('Not authenticated');
+    }
+  }
+
+  
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
